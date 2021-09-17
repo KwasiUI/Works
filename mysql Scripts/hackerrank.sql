790eb4da8e5e6a5aec7b5b3c72e036b6c3b3f22e
KwasiUI <58254743+KwasiUI@users.noreply.github.com>
2021-09-17 16:53:44 +0000

    Update hackerrank.sql

--- mysql Scripts/hackerrank.sql
+++ mysql Scripts/hackerrank.sql
@@ -38,3 +38,20 @@ select name
 from students 
 where marks > 75 
 order by substr(name,length(name)-2,3),id asc;
+
+/*
+Query an alphabetically ordered list of all names in OCCUPATIONS,
+immediately followed by the first letter of each profession as a parenthetical 
+(i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+
+Query the number of ocurrences of each occupation in OCCUPATIONS.
+Sort the occurrences in ascending order, and output them in the following format: 
+*/
+select concat(name,'(',substr(occupation,1,1),')') 
+from occupations
+order by name;
+
+select concat('There are a total of ', count(occupation),' ',lower(occupation),'s.')
+from occupations 
+group by occupation 
+order by count(occupation),occupation;
