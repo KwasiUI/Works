dcf805488ae6fdde5ea52035164393c47cfa5fd1
KwasiUI <58254743+KwasiUI@users.noreply.github.com>
2022-07-22 14:42:07 +0000

    Update System Queries.sql

--- MSSQL Scripts/System Queries.sql
+++ MSSQL Scripts/System Queries.sql
@@ -1,1 +1,6 @@
 select * from information_schema.columns  where column_name Like '%Last%'
+
+SELECT COLUMN_NAME AS 'Column_Name', TABLE_NAME AS 'Table_Name'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME ='draft_nbr'
+ORDER BY Table_Name, Column_Name
